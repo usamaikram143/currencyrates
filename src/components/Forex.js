935bc7b7f6108d41fd4579b06d4d6a24f4ffa995
usamaikram143 <usamaikram143@gmail.com>
2021-12-20 20:33:07 -0500

    working on forex page, fetched data from APollo using useQuery gettin exchange rate data from GetExchangeRates

--- src/components/Forex.js
+++ src/components/Forex.js
@@ -1,8 +1,36 @@
-const Forex=()=>{
-    return(
-        <div>
-            Forex
-        </div>
-    );
+import * as React from 'react';
+import { useQuery,gql } from '@apollo/client';
+
+
+const EXCHANGE_RATES = gql`
+query GetExchangeRates {
+  rates(currency: "USD") {
+    currency
+    rate
+  }
 }
+`;
+
+function ExchangeRates (){
+    const{ loading,error,data}=useQuery(EXCHANGE_RATES);
+    if(loading) return <p>Loading the Data...</p>;
+    if(error) return <p>There is somthing wrong, sorry for the inconvenient</p>
+
+    return data.rates.map(({currency,rate}) => (
+        <div key={currency}> Currency Symbol : {currency}
+        <div>
+            Exchange Rate $1 = {rate}{currency}
+        </div>
+        </div>
+    ));
+}
+
+const Forex = () => {
+    return (
+        <div>
+            <ExchangeRates/>
+        </div>
+    )
+}
+
 export default Forex;
