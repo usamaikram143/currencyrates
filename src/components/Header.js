800935f85b02d995eacf66dd399298e3552d19e8
awm907 <awm907@gmail.com>
2021-12-21 20:50:16 -0500

    Built the header component with navigating links

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,16 @@
-const Header=() => {
+import React from "react";
+import { NavLink } from "react-router-dom";
+import './Header.css';
+
+function Header(){
     return (
-      <div>Header</div>  
-    );
+        <nav>
+            <NavLink to="/" className="links" > Home </NavLink>
+            <NavLink to="/forex" className="links"> Forex</NavLink>
+            <NavLink to="/crypto" className="links"> Crypto </NavLink>
+            <NavLink to="/currency_converter" className="links"> Currency Converter </NavLink>
+        </nav>
+    )
 }
 
 export default Header;
