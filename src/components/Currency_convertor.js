de4f495c92f4e1ee6edad3e3524de6f5baa920f8
usamaikram143 <usamaikram143@gmail.com>
2021-12-21 20:39:01 -0500

    working on currency_convertor fetching data using API and using drop down printing it

--- src/components/Currency_convertor.js
+++ src/components/Currency_convertor.js
@@ -1,8 +1,45 @@
-const Currency_convertor=()=>{
-    return(
-        <div>
-            Currency
-        </div>
-    );
+import React from 'react';
+import { useEffect, useState } from 'react';
+import Axios from 'axios';
+import Dropdown from 'react-dropdown';
+import 'react-dropdown/style.css';
+
+const Currency_convertor = () => {
+  // Initializing all the state variables
+  const [info, setInfo] = useState([]);
+  const [from, setFrom] = useState("usd");
+  const [to, setTo] = useState("inr");
+  const [options, setOptions] = useState([]);
+
+  // Calling the api whenever the dependency changes
+  useEffect(() => {
+    Axios.get(
+`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${from}.json`)
+   .then((res) => {
+      setInfo(res.data[from]);
+    })
+  }, [from]);
+
+  useEffect(() => {
+    setOptions(Object.keys(info));
+  }, [info])
+
+ 
+ 
+
+  return (
+    <div>
+        <h3>From</h3>
+        <Dropdown options={options}
+            onChange={(e) => { setFrom(e.value) }}
+            value={from} placeholder="From" />
+        <h3>To</h3>
+        <Dropdown options={options}
+            onChange={(e) => {setTo(e.value)}}
+            value={to} placeholder="To" />
+        
+    </div>
+  );
 }
+
 export default Currency_convertor;
